35377033ea993198015672aeb3369867c39a7c2d
Samrakshan Karki <samrakshankarki1@gmail.com>
2025-08-21 21:43:05 +0545

    Extract job listings from merojob

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+# Merojob job listing url
+merojob_url = "https://merojob.com/services/top-job/"
+merojob_response = requests.get(merojob_url)
+
+merojob_html = merojob_response.text
+merojob_soup = BeautifulSoup(merojob_html, 'html.parser')
