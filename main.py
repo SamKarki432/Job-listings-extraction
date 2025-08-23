c060b1122bd1f9fd7fb6ae5c2a447dd41cada304
Samrakshan Karki <samrakshankarki1@gmail.com>
2025-08-23 21:46:09 +0545

    Extract job and employer description links

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+from urllib.parse import urljoin
 
 # Merojob job listing url
 merojob_url = "https://merojob.com/services/top-job/"
@@ -10,3 +11,20 @@ merojob_html = merojob_response.text
 merojob_soup = BeautifulSoup(merojob_html, 'html.parser')
 
 job_postings = merojob_soup.select('div[itemtype="http://schema.org/JobPosting"]')
+all_jobs = []
+merojob_listing_dict = {}
+for job in job_postings:
+    # Get link to job description
+    job_description_link = job.select('h1 a')[0]['href']
+    job_description_link = urljoin(merojob_url,job_description_link)
+
+    # Get link to employer profile  
+    employer_profile_link = job.select('h3 a')[0]['href']
+    employer_profile_link = urljoin(merojob_url,employer_profile_link)
+
+    job_links = {   "job_description_link": job_description_link,
+                    "employer_profile_link": employer_profile_link
+                    }
+    all_jobs.append(job_links)
+
+print(all_jobs)
